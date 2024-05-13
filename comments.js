a53357059fcc353d0efd23ee65748fee04b17071
dev-dsk <castrillojesus@gmail.com>
2024-05-13 00:37:15 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,34 @@
+// Create web server
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var fs = require('fs');
+var path = require('path');
+var commentsFile = path.join(__dirname, 'comments.json');
+
+// Serve static files
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+
+// Get comments
+app.get('/comments', function(req, res) {
+  fs.readFile(commentsFile, function(err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    res.setHeader('Cache-Control', 'no-cache');
+    res.json(JSON.parse(data));
+  });
+});
+
+// Add a comment
+app.post('/comments', function(req, res) {
+  fs.readFile(commentsFile, function(err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    var comments = JSON.parse(data);
+    var newComment = {
+      id: Date.now(),
